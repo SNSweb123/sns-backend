e7040aeb8039dc798b6191d59692d9607f4977a7
SNSweb123 <snswebofficial@gmail.com>
2026-05-14 15:07:47 +0530

    debug mongo

--- api/index.js
+++ api/index.js
@@ -1,13 +1,42 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import cors from 'cors';
+
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+
+// ✅ Root Route
 app.get("/", (req, res) => {
-  res.send("Backend Working 🚀");
+  res.send("Backend Running 🚀");
 });
+
+
+// ✅ Mongo Connect
+try {
+
+  await mongoose.connect(process.env.MONGODB_URI);
+
+  console.log("✅ MongoDB Connected");
+
+} catch (err) {
+
+  console.error("❌ MongoDB Error:");
+  console.error(err);
+}
+
+
+// ✅ TEMP TEST ROUTE
+app.get("/test", (req, res) => {
+  res.json({
+    success: true
+  });
+});
+
 
 export default app;
