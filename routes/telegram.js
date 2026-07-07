9aaa094b5ffd5f73188901aeabc3d09dc28ff1e5
SNSweb123 <snswebofficial@gmail.com>
2026-07-08 03:33:59 +0530

    debug telegram approval

--- routes/telegram.js
+++ routes/telegram.js
@@ -2,12 +2,85 @@ import express from "express";
 
 const router = express.Router();
 
-router.post("/webhook", (req,res)=>{
+router.post("/webhook", async(req,res)=>{
 
-  console.log("🔥 TELEGRAM WEBHOOK HIT");
-  console.log(req.body);
+try {
 
-  return res.status(200).send("OK");
+console.log("TELEGRAM DATA:", req.body);
+
+
+const callback = req.body.callback_query;
+
+
+if(!callback){
+    return res.sendStatus(200);
+}
+
+
+const action = callback.data;
+
+console.log("BUTTON DATA:", action);
+
+
+const [status, orderId] = action.split("_");
+
+
+console.log("STATUS:", status);
+console.log("ORDER ID:", orderId);
+
+
+
+const order = await Order.findOne({
+    orderId: orderId
+});
+
+
+console.log("FOUND ORDER:", order);
+
+
+
+if(!order){
+    return res.sendStatus(200);
+}
+
+
+
+if(status === "approve"){
+
+    order.status="approved";
+
+}
+
+
+if(status === "reject"){
+
+    order.status="rejected";
+
+}
+
+
+await order.save();
+
+
+console.log(
+"ORDER UPDATED:",
+order.status
+);
+
+
+
+res.sendStatus(200);
+
+
+
+}catch(error){
+
+console.log("WEBHOOK ERROR:",error);
+
+res.sendStatus(500);
+
+}
+
 
 });
 
