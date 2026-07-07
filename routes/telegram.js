3896a2ab13487951a4b5fbad16ccca5b0df25512
SNSweb123 <snswebofficial@gmail.com>
2026-07-08 03:20:12 +0530

    test telegram webhook

--- routes/telegram.js
+++ routes/telegram.js
@@ -1,62 +1,14 @@
 import express from "express";
-import Order from "../models/Order.js";
 
 const router = express.Router();
 
-
-router.post("/webhook", async (req, res) => {
+router.post("/webhook", (req,res)=>{
 
   console.log("🔥 TELEGRAM WEBHOOK HIT");
+  console.log(req.body);
 
-  res.status(200).json({
-    success:true
-  });
-
-  try {
-
-    const data = req.body;
-
-    const callback = data.callback_query;
-
-    if(!callback){
-      return;
-    }
-
-    const action = callback.data;
-
-    const [status, orderId] = action.split("_");
-
-
-    const order = await Order.findOne({
-      orderId
-    });
-
-
-    if(!order){
-      return;
-    }
-
-
-    if(status === "approve"){
-      order.status = "approved";
-    }
-
-
-    if(status === "reject"){
-      order.status = "rejected";
-    }
-
-
-    await order.save();
-
-
-  } catch(error){
-
-    console.log(error);
-
-  }
+  return res.status(200).send("OK");
 
 });
-
 
 export default router;
